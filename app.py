775e14dc41d3ad14b22b3976e70bba2420178b0b
Gorginio <hagoga.pr@gmail.com>
2022-05-27 00:02:29 +0300

    add highlighting

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-import json
-from types import SimpleNamespace
+import copy
+
+from flask import Flask, render_template, request
 
 from services.jsonParser import get_sudoku_array
 from services.sudokuService import solve_sudoku
@@ -17,14 +17,15 @@ def root():
 @app.route('/solve', methods=['POST'])
 def solve():
     data = request.form
-    sudoku_table = get_sudoku_array(data)
-    solve_sudoku(sudoku_table)
+    sudoku_data = get_sudoku_array(data)
+    solved_sudoku = copy.deepcopy(sudoku_data)
+    solve_sudoku(solved_sudoku)
     sudoku_render_table = {}
     for i in range(0, 9):
         for j in range(0, 9):
             key = str(i) + '-' + str(j)
-            sudoku_render_table[key] = sudoku_table[i][j]
-    return render_template('index.html', sudoku=sudoku_render_table)
+            sudoku_render_table[key] = solved_sudoku[i][j]
+    return render_template('index.html', sudoku_input=data, sudoku=sudoku_render_table)
 
 
 if __name__ == '__main__':
